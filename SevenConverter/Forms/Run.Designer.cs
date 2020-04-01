7beb490ec7aa27e880bb6416dfddd09393598d0d
Aleksandr Fediashov <fediachov@gmail.com>
2020-04-01 14:56:17 +0300

    add Run form

---
+++ SevenConverter/Forms/Run.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace SevenConverter.Forms
+{
+    partial class RunForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RunForm));
+            this.tbConsole = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbConsole
+            // 
+            resources.ApplyResources(this.tbConsole, "tbConsole");
+            this.tbConsole.BackColor = System.Drawing.Color.Black;
+            this.tbConsole.ForeColor = System.Drawing.Color.Lime;
+            this.tbConsole.Name = "tbConsole";
+            this.tbConsole.ReadOnly = true;
+            // 
+            // btnStop
+            // 
+            resources.ApplyResources(this.btnStop, "btnStop");
+            this.btnStop.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnStop.Name = "btnStop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // RunForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.tbConsole);
+            this.DoubleBuffered = true;
+            this.Name = "RunForm";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.RunForm_FormClosed);
+            this.Shown += new System.EventHandler(this.RunForm_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbConsole;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
