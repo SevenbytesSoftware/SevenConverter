c469a21119a2489d6857de570799d34d6298ac64
Aleksandr Fediashov <fediachov@gmail.com>
2020-04-04 13:18:46 +0300

    added an ability copying in clipboard a ffmpeg command line

--- SevenConverter/Forms/Run.Designer.cs
+++ SevenConverter/Forms/Run.Designer.cs
@@ -28,9 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RunForm));
             this.tbConsole = new System.Windows.Forms.TextBox();
             this.btnStop = new System.Windows.Forms.Button();
+            this.tbCommandLine = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.copyToClipboardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tbConsole
@@ -48,16 +53,40 @@
             this.btnStop.Name = "btnStop";
             this.btnStop.UseVisualStyleBackColor = true;
             // 
+            // tbCommandLine
+            // 
+            resources.ApplyResources(this.tbCommandLine, "tbCommandLine");
+            this.tbCommandLine.BackColor = System.Drawing.Color.Black;
+            this.tbCommandLine.ContextMenuStrip = this.contextMenuStrip1;
+            this.tbCommandLine.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.tbCommandLine.Name = "tbCommandLine";
+            this.tbCommandLine.ReadOnly = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToClipboardToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            resources.ApplyResources(this.contextMenuStrip1, "contextMenuStrip1");
+            // 
+            // copyToClipboardToolStripMenuItem
+            // 
+            this.copyToClipboardToolStripMenuItem.Name = "copyToClipboardToolStripMenuItem";
+            resources.ApplyResources(this.copyToClipboardToolStripMenuItem, "copyToClipboardToolStripMenuItem");
+            this.copyToClipboardToolStripMenuItem.Click += new System.EventHandler(this.copyToClipboardToolStripMenuItem_Click);
+            // 
             // RunForm
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tbCommandLine);
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.tbConsole);
             this.DoubleBuffered = true;
             this.Name = "RunForm";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.RunForm_FormClosed);
             this.Shown += new System.EventHandler(this.RunForm_Shown);
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -67,5 +96,8 @@
 
         private System.Windows.Forms.TextBox tbConsole;
         private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox tbCommandLine;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem copyToClipboardToolStripMenuItem;
     }
 }
